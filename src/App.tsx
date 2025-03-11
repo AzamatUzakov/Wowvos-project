24fa1d4141275f0c89e7f9462f21f322f8bda378
AzamatUzakov <azamatakmal053@gmail.com>
2025-03-11 16:54:00 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+function App() {
+
+  return (
+    <>
+      <div className="main-container flex">
+        <Navbar />
+        <div className="right-column">
+          <Header />  
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface HeaderProps {
+
+}
+
+const Header: React.FC<HeaderProps> = () => {
+    return (
+        <header>
+<h1>Header</h1>
+        </header>
+
+    );
+}
+
+export default Header;
