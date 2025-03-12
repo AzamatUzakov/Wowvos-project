74cb6c4c43194ac6029688ffe7548cad4ac74bc8
AzamatUzakov <azamatakmal053@gmail.com>
2025-03-12 15:29:57 +0500

    1.6

--- src/components/section/Login.tsx
+++ src/components/section/Login.tsx
@@ -4,12 +4,17 @@ import React from "react";
 const LogIn: React.FC = () => {
     return (
 
-        <div className="relative">
+        <div className="relative w-full h-screen text-center">
 
-            <div>
-                <img src="/mainLogo.svg" alt="logo" />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <img src="/mainLogo.svg" alt="logo" className="w-[205px] mb-[70px] mx-auto" />
 
-                <h1>Sign in to your account</h1>
+                <h1 className="text-3xl font-semibold">Sign in to your account</h1>
+
+                <form>
+
+                </form>
+
             </div>
 
         </div>
